b3e9107bbb6a53082dffc0258b4c54afc00163b7
blake-mcguire <blake.lm@outlook.com>
2024-06-10 15:17:44 -0500

    First Commit to backend

---
+++ week1/mon/syntax_hw.py
@@ -0,0 +1,43 @@
+#MY DANCE MOVES
+# TASK #1
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+    
+# TASK #2
+
+mood = lower(str(input("How are you feeling today:")))
+
+if mood == "happy":
+    print("thats great to hear!")
+elif mood == "sad":
+    print("Im sorry, I hope your day gets better")
+else:
+    print("Wow, look at you covering the full spectrum of human emotions, good for you, maybe...")
+    
+
+#PYTHON DATA TYPES AND type()  function
+#Task 1
+
+variable_a = "Hello, World!"
+variable_b = 23
+variable_c = 3.14
+variable_d = True
+
+print(type(variable_a))
+print(type(variable_b))
+print(type(variable_c))
+print(type(variable_d))
+
+#ARITHMETIC IN DAILY LIFE
+
+bread = 2.75
+peanutbutter= 4.50
+jelly = 3.23
+
+total_cost = bread + peanutbutter + jelly
+print(total_cost)
+
