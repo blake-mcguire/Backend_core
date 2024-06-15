a6dca0df033285d2da74cc63e74eb92e639ce9d3
blake-mcguire <blake.lm@outlook.com>
2024-06-15 16:46:58 -0500

    TO-DO List Application

---
+++ week1/weekend/To-Do.py
@@ -0,0 +1,118 @@
+task_list = []                                          #defines list variables
+completed_tasks = []
+
+
+def add_task():                              #Adds Task to a task list 
+    try:
+        task = input("Enter the Task you would like to add!: ")
+        due_date = input("Enter The Due Date: ")
+        task_list.append((task, due_date))
+        print(f"You added {task} to your task list!")
+        
+    except Exception as e:
+        print(f"An Error Occured: {e}")                               
+    
+    
+    
+def view_task():                                          #Shows the view of tasks that are still open
+    if task_list:   
+        for task in task_list:
+            print(f"""-----------------------------------
+Task: {task[0]} -- Due: {task[1]}""")
+    else:
+        print("Your Task List is empty.")
+    
+        
+
+def complete_task():                                   #Marks a task as complete
+    if task_list:
+        
+        for index, (task, due_date) in enumerate(task_list):
+            print(f"""------------------------------------
+{index}: {task}; {due_date}""")
+        
+        try:    
+            complete = input("Enter The Index of The Task You would like to mark complete: ")
+            complete_task = task_list.pop(int(complete))
+            completed_tasks.append(complete_task)
+            print(f"You Completed {complete_task[0]}!")
+        
+        except ValueError:
+            print("please enter the index!")
+    
+    else:
+        print("your task list is empty")
+        
+
+
+def view_completed_tasks():                                 #Shows The list of Tasks That Have Been Completed
+    if completed_tasks:   
+        for task in completed_tasks:
+            print(f"Task: {task[0]} - Status: Complete")
+    else:
+        print("you have no completed tasks")
+
+
+def delete_task():                                          #deletes a task from the task list
+    if task_list:
+        for index, (task, due_date) in enumerate(task_list):
+            print(f"{index}: {task}; {due_date}")
+        
+        try:
+            del_task = input("Enter the index of the task you would like to delete: ")
+            task_list.pop(int(del_task))
+        
+        except ValueError:
+            print("Please enter a valid Index")
+    
+    else:
+        print("Your Task List is empty!")
+    
+
+
+def main():                                                 #Driver function
+    while True:
+        
+        print("""
+    Welcome to the To-Do list!
+    
+    1. Add Task
+    2. View Tasks
+    3. Mark a task as complete
+    4. View Completed Tasks
+    5. Delete a task
+    6. Quit
+                        """)
+        
+        choice = input("Enter the Corresponding number of the action you would like to execute: ")
+        # If Choice == 1,  runs the add task function
+        
+        if choice == "1":
+            add_task()                              
+        # choice 2: simply runs the view task function
+        
+        elif choice == '2':
+            view_task()
+            
+        # choice 3: runs the complete task function 
+        elif choice == '3':
+            complete_task()
+        # choice 4: runs view completed task function
+        
+        elif choice == '4':
+            view_completed_tasks()
+            
+        # choice 5: runs the delete task func
+        
+        elif choice == '5':
+            delete_task()
+        # exits function
+        
+        elif choice == '6':
+            print('Exiting...')
+            break
+        
+        else:
+            print('Please Enter a Valid Option.')
+        
+main()
